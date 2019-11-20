ff95854be8d2f73879679acb8a86bef02a34a1cf
minhbc83 <40559356+minhbc83@users.noreply.github.com>
2019-11-20 09:54:16 +0700

    Add files via upload

---
+++ admin/template/navigation.php
@@ -0,0 +1,15 @@
+
+	<div class="row">
+		<div class="col-md-3">
+			<nav class="navbar navbar-light bg-light">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="../index.php">Trang chủ</a>
+						<a class="nav-link" href="index.php">Dash Board</a>
+						<a class="nav-link" href="view_product.php">Danh sách sản phẩm</a>
+						<a class="nav-link" href="create_product.php">Tạo sản phẩm mới</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="col-md-9">
